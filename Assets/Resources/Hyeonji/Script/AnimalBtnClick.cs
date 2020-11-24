5fab9bbb631d9f85de38cb44b68b35b92fbf95fc
Animal-Defence <whguswl0408@gmail.com>
2020-11-25 03:49:14 +0900

    동물 설정 메뉴 틀 제작

---
+++ Assets/Resources/Hyeonji/Script/AnimalBtnClick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalBtnClick : MonoBehaviour
+{
+    public Sprite AnimalSprite;
+    public Button SelectedBtn1;
+    public Button SelectedBtn2;
+    public Button SelectedBtn3;
+
+    public void ChangeImage()
+    {
+        if (SelectedBtn1.image.sprite.name == "XbtnImg")
+        {
+            SelectedBtn1.image.sprite = AnimalSprite;
+        }
+        else if(SelectedBtn2.image.sprite.name == "XbtnImg")
+        {
+            SelectedBtn2.image.sprite = AnimalSprite;
+        }
+        else if(SelectedBtn3.image.sprite.name == "XbtnImg")
+        {
+            SelectedBtn3.image.sprite = AnimalSprite;
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ Assets/Resources/Hyeonji/Script/SelectedBtnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectedBtnClick : MonoBehaviour
+{
+    public Sprite XSprite;
+    private Image myImage;
+
+    private void Start()
+    {
+        myImage = GetComponent<Image>();
+    }
+
+    public void ChangeImage()
+    {
+        myImage.sprite = XSprite;
+        
+    }
+
+}
