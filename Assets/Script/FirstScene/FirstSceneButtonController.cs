ed7162138aca7f9bcad5dd23adb8216f94bba00a
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2020-11-26 03:38:17 +0900

    메인씬 버튼 이벤트
    데이터삭제 버튼 누르면 정말 삭제할껀지 물어보는 뷰가 뜨고, 예 아니오 고를수있다.
    개발자정보도 버튼 누르면 개발자 정보 뷰가 뜨게 해놨는데 내용은 아직 안채움!

--- Assets/Script/FirstScene/FirstSceneButtonController.cs
+++ Assets/Script/FirstScene/FirstSceneButtonController.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class FirstSceneButtonController : MonoBehaviour
 {
-
+    public GameObject ClearDataView;
+    public GameObject DeveloperInfoView;
     public void onClickStartGameBtn()
     {
         SceneManager.LoadScene("Game");
@@ -14,13 +15,29 @@ public class FirstSceneButtonController : MonoBehaviour
 
     public void onClickClearDataBtn()
     {
-        PlayerPrefs.DeleteAll();
+        ClearDataView.SetActive(true);
     }
     public void onClick_DeveloperInfo_Btn()
     {
         readDeveloperInfo();
+        DeveloperInfoView.SetActive(true);
     }
 
+    public void onClickCancleBtn()
+    {
+        DeveloperInfoView.SetActive(false);
+    }
+
+    public void onClickYesBtn()
+    {
+        PlayerPrefs.DeleteAll();
+        ClearDataView.SetActive(false);
+        Application.Quit();
+    }
+    public void onClickNoBtn()
+    {
+        ClearDataView.SetActive(false);
+    }
     public void readDeveloperInfo()
     {
         var json = PlayerPrefs.GetString("game_info");//파일 이미 만들어져 있기 때문에 null처리안함
