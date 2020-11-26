9bda52398e198428e474df3917828c105a9c6f82
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2020-11-27 00:47:14 +0900

    11_27 수정
    1. 적들과 보스들 체력 증가 수정
    2. 플레이어 HP 기능, 게임오버 기능 완료
    -HP는 3. 0되어도 한번의 기회가 있다.
    3 2 1 0 후에 맞으면 게임오버
    -게임오버 화면 죽인 보스, 적 수 나온다
    3. 개발자 정보 수정
    4. 게임종료 버튼 수정

---
+++ Assets/Script/PlayerHP.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PlayerHP : MonoBehaviour
+{
+    int playerHP = 3; // HP다 사라졌을때 죽어야 게임오버 될 수 있도록
+    //public GameObject[] HPtransforms;
+    public Image[] HPImage;
+    public GameObject GameOverView;
+    private void Start()
+    {
+        string minute = DateTime.Now.ToString("mm");
+        AnimalArr.callAnimalArr();
+        int rannum = int.Parse(minute)%AnimalArr.AnimalArray.Length;
+        Debug.Log("Image " + AnimalArr.AnimalArray[rannum]);
+        //AnimalImage.sprite = Resources.Load<Sprite>(string.Format("Animals/{0}", missionData.animal_sprite_name));
+        HPImage[0].sprite = Resources.Load<Sprite>(string.Format("Animals/{0}", AnimalArr.AnimalArray[rannum]));
+        HPImage[1].sprite = Resources.Load<Sprite>(string.Format("Animals/{0}", AnimalArr.AnimalArray[rannum]));
+        HPImage[2].sprite = Resources.Load<Sprite>(string.Format("Animals/{0}", AnimalArr.AnimalArray[rannum]));
+    }
+
+    public void MinusHP()
+    {
+        if (playerHP <= 0)
+        {
+            GameOverView.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            Destroy(HPImage[playerHP - 1]);
+            playerHP--;
+        }
+    }
+
+
+}
