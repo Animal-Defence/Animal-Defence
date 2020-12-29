64c033f371148089f5a468983789fae16d782e44
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2020-12-29 20:50:36 +0900

    추가 수정
    1. 게임씬 미션버튼 비활성화
    2. 스테이지씬 미션버튼 활성화
    -미션 스크립트 수정예정
    3. 자잘한 UI,스크립트 수정

--- Assets/Script/GoMissionMenuBtn.cs
+++ Assets/Script/GoMissionMenuBtn.cs
@@ -11,13 +11,18 @@ public class GoMissionMenuBtn : MonoBehaviour
     {
         MissionMenuView.SetActive(true);
         Time.timeScale = 0.0f;
+        //이제 미션 메뉴를 누를때마다 부르는게 아니라 스테이지 고르는 씬에 입장했을때만 실행되도록
+        //게임끝나면 메인화면으로 넘어가게? 스테이지 선택으로 넘어가게?
+        SettingMissionData();
+    }
+
+   public void SettingMissionData()
+    {
         GameObject.Find("EnemyDeathManager").GetComponent<EnemyDeathManager>().setNewEnemyDeath();
         GameObject.Find("Player_Coin").GetComponent<Player_Coin>().setNowPlayerCoin();
         GameObject.Find("TestUGUI").GetComponent<TestUGUI>().setGamePlayCount();
         GameObject.Find("TestUGUI").GetComponent<TestUGUI>().setReadDeveloperInfo();
         GameObject.Find("TestUGUI").GetComponent<TestUGUI>().testUBUISetting();
     }
-
-    
 
 }
---
+++ Assets/Script/Sub/HomeButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeButton : MonoBehaviour
+{
+    public void onClickButton()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
